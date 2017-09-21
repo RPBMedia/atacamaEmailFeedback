072fbc79d3cd100b0a664dbd1716a8f91948ae6a
Rui Baiao <rui@frostdigital.se>
2017-09-21 17:01:27 +0200

    passport auth flow complete

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+//Order of the require statements matters. User schema must be created
+//Before we use passport
+require('./models/User');
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
 const keys = require('./config/keys');
@@ -7,6 +12,17 @@ const keys = require('./config/keys');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    //Cookies will expire after 30 days
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+//Handler to the users collection
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +14,18 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-
-      console.log('accessToken:', accessToken);
-      console.log('refreshToken:', refreshToken);
-      console.log('profile:', profile);
+      //Save new user record into mongoDB
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          console.log('User already exists. No new record will be created');
+          done(null, existingUser);
+        } else {
+          console.log('New user detected! Creating new record...');
+          new User({ googleId: profile.id }).save().then(user => {
+            done(null, user);
+          });
+        }
+      });
     }
   )
 );
